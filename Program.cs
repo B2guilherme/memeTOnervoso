5d4e12d6a329399d09f643c8596e92c4f83ed3f3
B2guilherme <guilhermecrm18@gmail.com>
2026-04-10 20:23:58 -0300

    memeTOnervoso

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿﻿string fraseDigitada, fraseMemeToNervoso;
+Console.Write("Voce que esta atras desta tela, oque achas de mim? ");
+fraseDigitada = Console.ReadLine()!;
+
+fraseMemeToNervoso =
+    $"{fraseDigitada.Remove(10)}... NÃO, PERA.\nTô nervoso";
+
+Console.WriteLine(fraseMemeToNervoso);
